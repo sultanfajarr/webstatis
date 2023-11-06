d24ea6f1dcfa1f5cd13421067f3cde6fa64aade2
sultan <sultan.fajar21@mhs.uinjkt.ac.id>
2023-11-06 14:01:22 +0700

    update uts

--- UTS.js
+++ UTS.js
@@ -1,1 +1,10 @@
-alert("Silahkan Memesan Menu Kami")
+
+alert('Selamat Datang')
+
+const menu = document.querySelector('.navbar #menu');
+const nav = document.querySelector('.navbar .navbar-nav');
+
+menu.addEventListener('click', function() {
+    nav.classList.toggle('active')
+})
+
